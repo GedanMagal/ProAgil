7e6d2919a3e2d2be37c6887de643e51aec98c6cb
Gedan Magalhães <gedanycar@gmail.com>
2020-01-04 20:25:47 -0300

    Adicionado Projeto Angular

--- ProAgil.Api/Startup.cs
+++ ProAgil.Api/Startup.cs
@@ -24,6 +24,7 @@ namespace ProAgil.Api
                 x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection"))
             );
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +41,7 @@ namespace ProAgil.Api
             }
 
             // app.UseHttpsRedirection();
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseMvc();
         }
     }
